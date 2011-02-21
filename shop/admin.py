bf178b12215c0e7dab7817faba4fd673266474ca
Simon Connah <simon.connah@googlemail.com>
2011-02-21 13:46:33 +0000

    Cleaned up the admin Order display.

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from dropship.shop.models import ExtendedProfile, Order, Cart
+from dropship.shop.models import ExtendedProfile, Order, OrderItem, Cart
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+    readonly_fields = ('item', 'price', 'quantity', 'address', 'other_cost',
+            'total',)
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderItemInline,
+    ]
+
+    readonly_fields = ('billing', 'total',)
 
 admin.site.register(ExtendedProfile)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
