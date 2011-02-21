08928f4d4b05d53dc0e0b6dd6c711e0a8da64e58
Simon Connah <simon.connah@googlemail.com>
2011-02-21 13:46:45 +0000

    Admin tracking url.

--- shop/urls.py
+++ shop/urls.py
@@ -6,6 +6,7 @@ urlpatterns = patterns('',
     (r'^order/(?P<order_id>\d+)/$', 'dropship.shop.views.order.order_by_id'),
     (r'^order/complete/(?P<order_id>\d+)/$', 'dropship.shop.views.order.complete'),
     (r'^order/tracking/(?P<user_id>\d+)/$', 'dropship.shop.views.order.order_tracking'),
+    (r'^order/tracking/admin/$', 'dropship.shop.views.admin_tracking.admin_order_tracking'),
     (r'^edit/shippingaddress/(?P<item_id>\d+)/$', 'dropship.shop.views.address.ship_edit'),
     (r'^edit/billingaddress/(?P<user_id>\d+)/$', 'dropship.shop.views.address.bill_edit'),
 )
